fc146ccb6dcdf29e10340103bdc6493ad5eeb236
Kristoffer Jansen <kristoffer@smartdok.com>
2022-05-07 22:05:47 +0200

    Set up express server for pinging bot

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,38 @@
+import express from 'express';
 import { Guild } from 'discord.js';
 import { discord } from './discord';
 import { events } from './events';
 import { GuildManager } from './managers/guildManager';
 import { IErrorContext, logError } from './sentry';
 
-const connectedGuilds: Map<string, GuildManager> = new Map();
-const client = discord;
+const PORT = process.env.OMEGABOT_PORT;
 
-client.once('ready', () => {
-    client.guilds.cache.forEach((guild: Guild) => {
-        try {
-            const newGuild = new GuildManager(guild);
-            connectedGuilds.set(guild.id, newGuild);
-        } catch (e) {
-            const context: IErrorContext = {
-                name: 'Client startup',
-                ctx: null,
+const app = express();
+
+const startServer = async () => {
+    const connectedGuilds: Map<string, GuildManager> = new Map();
+    const client = discord;
+
+    client.once('ready', () => {
+        client.guilds.cache.forEach((guild: Guild) => {
+            try {
+                const newGuild = new GuildManager(guild);
+                connectedGuilds.set(guild.id, newGuild);
+            } catch (e) {
+                const context: IErrorContext = {
+                    name: 'Client startup',
+                    ctx: null,
+                }
+                
+                logError(e, context);
             }
-            
-            logError(e, context);
-        }
+        });
+        events(client, connectedGuilds);
+        console.log('Ready');
     });
-    events(client, connectedGuilds);
-    console.log('Ready');
-});
+
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+}
+
+startServer();
+
